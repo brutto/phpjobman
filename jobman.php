52889d5e0e590044be792b03f6445b36ea8f3b8f
jayridge <jayridge@bf4be068-a440-0410-a82d-2ff7e836fc59>
2007-12-07 17:07:38 +0000

    Moved reaper outside of concurrency check so we don't have any zombies.

--- jobman.php
+++ jobman.php
@@ -29,39 +29,40 @@ declare(ticks = 1);
 openlog('phpjobman', LOG_PID, LOG_USER);
 $children = array();
 
-while(true) {
-    if (count($children) >= $concurrency) {
-        foreach ($children as $child => $start) {
-            if (!posix_kill($child, 0)) {
-                $elapsed = microtime(true) - $children[$child];
-                pcntl_waitpid($child, $status);
-                if (pcntl_wifexited($status)) {
-                    $es = pcntl_wexitstatus($status);
-                    syslog(LOG_NOTICE, "$shellscript exit($es) seconds($elapsed)");
-                } else {
-                    $msg = "child($child) abnormal exit";
-                    if (pcntl_wifsignaled($status)) {
-                        $msg .= " : exited with signal " . pcntl_wtermsig($status);
-                    }
-                    syslog(LOG_WARNING, $msg);
-                }
-                unset($children[$child]);
-            } else if (microtime(true) - $start > $runseconds) {
-                $res = posix_kill($child, SIGTERM);
-                syslog(LOG_ERR, "$shellscript exceeded $runseconds seconds");
-            }
-        }
-        sleep($sleepinterval);
-    } else {
+while (true) {
+    if (count($children) <= $concurrency) {
         $pid = pcntl_fork();
         if ($pid == -1) {
             die("pcntl_fork failed");
-        } else if ($pid == 0) {    // child
+        } else if ($pid == 0) {
+            /* child */
             system($shellscript, $ret);
             exit($ret);
-        } else {    // parent
-           $children[$pid] = microtime(true); // Push the PID of the created child into $children
+        } else {
+            /* parent */
+            $children[$pid] = microtime(true);
         }
     }
+    foreach ($children as $child => $start) {
+        if (!posix_kill($child, 0)) {
+            $elapsed = microtime(true) - $children[$child];
+            pcntl_waitpid($child, $status);
+            if (pcntl_wifexited($status)) {
+                $es = pcntl_wexitstatus($status);
+                syslog(LOG_NOTICE, "$shellscript exit($es) seconds($elapsed)");
+            } else {
+                $msg = "child($child) abnormal exit";
+                if (pcntl_wifsignaled($status)) {
+                    $msg .= " : exited with signal " . pcntl_wtermsig($status);
+                }
+                syslog(LOG_WARNING, $msg);
+            }
+            unset($children[$child]);
+        } else if (microtime(true) - $start > $runseconds) {
+            $res = posix_kill($child, SIGTERM);
+            syslog(LOG_ERR, "$shellscript exceeded $runseconds seconds");
+        }
+    }
+    sleep($sleepinterval);
 }
 ?>
